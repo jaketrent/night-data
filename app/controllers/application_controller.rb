a1effb9d0a59ec8349f4475fb0f0d6c1435bed15
jaketrent <trent.jake@gmail.com>
2014-01-15 09:05:57 -0700

    store acceptable domains

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::API
   before_filter :set_access
 
+
   def set_access
-    response.headers["Access-Control-Allow-Origin"] = "http://localhost:3000 http://localhost:5000"
-    response.headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-    response.headers['Access-Control-Request-Method'] = '*'
-    response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    allowed_domains = %w(http://localhost:5000 http://localhost:3000 http://thisisthenight.herokuapp.com)
+    if allowed_domains.include? request.headers['HTTP_ORIGIN']
+      response.headers["Access-Control-Allow-Origin"] = request.headers['HTTP_ORIGIN']
+      response.headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+      response.headers['Access-Control-Request-Method'] = '*'
+      response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+      response.headers['Access-Control-Allow-Credentials'] = 'true'
+    end
   end
 end
