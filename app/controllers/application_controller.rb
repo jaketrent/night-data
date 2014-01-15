51ebe098baa4cff1c478179cfed359fe892ac1c6
jaketrent <trent.jake@gmail.com>
2014-01-15 08:04:34 -0700

    add access-control headers for methods and headers to get http options, etc to work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,8 @@ class ApplicationController < ActionController::API
 
   def set_access
     response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    response.headers['Access-Control-Request-Method'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
